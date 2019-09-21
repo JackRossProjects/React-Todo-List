800506fbf91854979c33f0a92e1dc91544d5dad1
Jack Ross <43938405+JackRossProjects@users.noreply.github.com>
2019-09-21 00:42:11 -0600

    React app

---
+++ build/precache-manifest.ad2225e4d5654219c265ffcc1a77cde2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a408296814a87b2a43209c7c51847e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2357147dde991158c36a",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "83f9ca9418e4f55c879f",
+    "url": "/static/js/2.56523b0d.chunk.js"
+  },
+  {
+    "revision": "2357147dde991158c36a",
+    "url": "/static/js/main.b2666a8b.chunk.js"
+  },
+  {
+    "revision": "ff7e631346c12c639492",
+    "url": "/static/js/runtime-main.7677ad42.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import uuid from 'uuid'
+import About from './components/pages/About'
+import Header from './components/layouts/Header';
+import Todo from './components/Todo';
+import AddTodo from './components/AddTodo'
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: []
+  }
+
+  // Toggle complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  // Delete todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+
+  // Add todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
+
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <div className="container">
+          <Header />
+          <Route exact path='/' render={props => (
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo}/>
+              <Todo todos={this.state.todos} markComplete={this.markComplete}
+              delTodo={this.delTodo}/>
+            </React.Fragment>
+          )} />
+          <Route path="/about" component={About}/>
+        </div>
+      </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
